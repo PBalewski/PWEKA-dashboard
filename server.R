2204f1b14d70b0ceb2368db24354ce4d503bd3b3
PBalewski <balewskipiotrek@gmail.com>
2024-06-07 22:13:51 +0200

    PWEKA almost done

--- server.R
+++ server.R
@@ -19,11 +19,41 @@ library(caret)
 library(mlr)
 library(DT)
 library(DALEX)
+library(shinythemes)
+library(pROC)
+library(plotly)
 
 
 function(input, output, session) {
   
   data <- read.csv("./data/AIDS_Classification.csv")
+  data <- data %>%
+    rename("treatment" = "trt")
+  data <- data %>%
+    rename("weight" = "wtkg")
+  data <- data %>%
+    rename("hemophilia" = "hemo")
+  data <- data %>%
+    rename("homosexual_activity" = "homo")
+  data <- data %>%
+    rename("karnofsky_score" = "karnof")
+  data <- data %>%
+    rename("days_before_anti_retrovial" = "preanti")
+  data <- data %>%
+    rename("non_white" = "race")
+  data <- data %>%
+    rename("antiretroviral_history" = "str2")
+  data <- data %>%
+    rename("treatment_ZDV" = "treat")
+  data <- data %>%
+    rename("CD4_baseline" = "cd40")
+  data <- data %>%
+    rename("CD4_after_20days" = "cd420")
+  data <- data %>%
+    rename("CD8_baseline" = "cd80")
+  data <- data %>%
+    rename("CD8_after_20days" = "cd820")
+
   set.seed(42)
   sample_index <- sample(1:nrow(data), 0.7 * nrow(data))
   train_data <- data[sample_index, ]
@@ -60,88 +90,152 @@ function(input, output, session) {
     
     conf_matrix_plot <- ggplot(plt, aes(Prediction, Reference, fill = Freq)) +
       geom_tile() + geom_text(aes(label = Freq)) +
-      scale_fill_gradient(low = "white", high = "red") +
+      scale_fill_gradient(low = "white", high = "blue") +
       labs(x = "Reference", y = "Prediction") +
       scale_x_discrete(labels = c("infected", "healthy")) +
-      scale_y_discrete(labels = c("healthy", "infected"))
+      scale_y_discrete(labels = c("healthy", "infected")) +
+      theme_minimal()
     
     conf_matrix_plot
   })
   
-  learning_curves <- reactive({
-    learners <- list()
-    if ("rf" %in% input$chooseAlgorithms) {
-      learners <- c(learners, makeLearner("classif.randomForest", predict.type = "response", ntree = 100, id="randomForest"))
-    } 
-    if ("dt" %in% input$chooseAlgorithms) {
-      learners <- c(learners, makeLearner("classif.rpart", predict.type = "response", id="decision tree"))
-    } 
-    if ("lr" %in% input$chooseAlgorithms) {
-      learners <- c(learners, makeLearner("classif.logreg", predict.type = "response", id="logistic regression"))
-    } 
-    if ("nn" %in% input$chooseAlgorithms) {
-      learners <- c(learners, makeLearner("classif.nnet", predict.type = "response", size = 5, trace = FALSE, id="neural network"))
-    } 
-    if ("svm" %in% input$chooseAlgorithms) {
-      learners <- c(learners, makeLearner("classif.svm", predict.type = "response", id="SVM"))
-    }
+  output$ROCcurve <- renderPlot({
+    predictions <- model_predictions()
+    # Create ROC curve
+    rocobj <- plot.roc(test_data$infected, as.numeric(predictions),
+                      percent=TRUE,
+                       ci=TRUE, xlim=c(100,0)) # Adjusted x-axis limits
     
-    return(learners)
+    # Print the AUC (will contain the CI)
+    ciobj <- ci.se(rocobj, 
+                   specificities=seq(0, 100, 5)) # Calculate CI of sensitivity
+    
+    # Plot CI as a shape
+    plot(ciobj, type="shape", col="#1c61b6AA") # Blue shape
+    
+    # Plot CI of thresholds
+    plot(ci(rocobj, of="thresholds", thresholds="best"))
   })
   
-  output$learning_curves <- renderPlot({
-    learners <- learning_curves()
-    if (length(learners) > 0) {
-      train_task <- makeClassifTask(data = train_data, target = "infected")
-      measure <- acc
-      rdesc <- makeResampleDesc("CV", iters = 10)
-      learning_curve_data <- generateLearningCurveData(
-        learners = learners, 
-        task = train_task, 
-        percs = seq(0.1, 1.0, by = 0.1),  # Training set sizes from 10% to 100%
-        measures = measure, 
-        resampling = rdesc
-      )
-      p <- plotLearningCurve(learning_curve_data) +
-        ggtitle("Learning Curves for Various Classifiers with 10-Fold Cross-Validation") +
-        theme_minimal()
-      
-      print(p)
-    } else {
-      plot.new()
-      text(0.5, 0.5, "No algorithm selected", cex = 1.5)
-    }
-  })
-  
-  
+
   output$dataTableOutput <- renderDT({
     dt_data = data[][-c(14, 15, 16, 17, 18, 19, 20, 21, 22)]
     datatable(dt_data, filter = 'top', options = list(
       pageLength = 5, autoWidth = TRUE, selection = "single"))
   })
   
+  get_model <- reactive({
+    input_model <- input$selectAlgorithm2
+    
+    if (input_model == "Random Forest") {
+      model <- randomForest(infected ~ ., data = train_data, ntree = 100)
+    } else if (input_model == "Decision Tree") {
+      model <- rpart(infected ~ ., data = train_data, method = "class")
+    } else if (input_model == "Logistic Regression") {
+      model <- glm(infected ~ ., data = train_data, family = binomial)
+    } else {
+      model <- nnet(infected ~ ., data = train_data, size = 5)
+    }
+    
+    return(model)
+  })
   
   output$selectedRowPlot <- renderPlot({
     selected_row <- input$dataTableOutput_rows_selected
     if (length(selected_row) == 1) {
-      rf_model <- randomForest(infected ~ ., data = train_data, ntree = 100)
-      explain_rf <- DALEX::explain(model = rf_model,
+      model <- get_model()
+      explain <- DALEX::explain(model = model,
                                    data = data[-23],
                                    y = data$infected == "1", 
-                                   label = "Random Forest")
+                                   label = input$selectAlgorithm2)
       record = data[selected_row,][-23]
-      bd_rf <- predict_parts(explainer = explain_rf,
+      bd_rf <- predict_parts(explainer = explain,
                              new_observation = record,
                              type = "break_down")
       plot(bd_rf)
     } else if (length(selected_row) > 1){
       plot.new()
       text(0.5, 0.5, "Please select a single row", cex = 1.5)
-    } else {
-      plot.new()
-      text(0.5, 0.5, "Select a row from the table to see the features importance analysis", cex = 1.5)
     }
   })
   
+  
+  plot_dist <- function(attribute) {
+    # Create a data frame with the column referenced by attribute
+    df <- data.frame(value = data[[attribute]])
+    
+    # Plot density using ggplot2 with a dynamic title
+    p<-ggplot(df, aes_string(x = "value")) +
+      geom_density(fill = "blue", alpha = 0.3) +
+      ggtitle(paste("Density plot of attribute", attribute)) +
+      xlab(paste(attribute)) +
+      ylab("Density") +
+      theme_minimal()
+    
+    print(p)
+  }
+  
+  plot_dist_discrete <- function(attribute) {
+    # Create a data frame with the column referenced by attribute
+    df <- data.frame(value = data[[attribute]])
+    
+    # Calculate the x-axis limits
+    
+    # Plot bar chart using ggplot2 with a dynamic title and specified x-axis limits
+    p <- ggplot(df, aes_string(x = "value")) +
+      geom_bar(fill = "blue", alpha = 0.3) +
+      ggtitle(paste("Bar chart of attribute", attribute)) +
+      xlab(paste(attribute)) +
+      ylab("Frequency") +
+      scale_x_continuous(breaks = seq(min(df$value), max(df$value), by = 1)) + 
+      theme_minimal()
+    
+    print(p)
+  }
+  
+  input_column <- reactive({
+    column <- input$selectColumn
+  })
+  
+  numeric_attr = c("time", "age", "weight", "days_before_anti_retrovial", "CD4_baseline", "CD4_after_20days", "CD8_baseline", "CD8_after_20days")
+  output$selectedColumnPlot <- renderPlot({
+    attribute <- input_column()
+    if (attribute %in% numeric_attr) {
+      plot_dist(attribute)
+    }
+    else {
+      plot_dist_discrete(attribute)
+    }
+  })
+  
+  
+  input_attribute <- reactive({
+    attribute <- input$selectAttribute
+  })
+  
+  output$violinPlot <- renderPlot({
+    data$infected <- as.factor(data$infected)
+    attribute <- input_attribute()
+    p_violin <- ggplot(data, aes(x = infected, y = .data[[attribute]], fill = infected)) +
+      geom_violin(trim = FALSE) +
+      scale_fill_manual(values = c("0" = "dodgerblue", "1" = "tomato")) +
+      theme_minimal() +
+      labs(x = "infected", y = attribute, title = paste("Violin Plot of", attribute, "by Infected Status"))
+    
+    p_violin
+  })
+  
+  
+  output$Boxplot <- renderPlot({
+    data$infected <- as.factor(data$infected)
+    attribute <- input_attribute()
+    ggplot(data, aes(x = infected, y = .data[[attribute]], fill = infected)) +
+      geom_boxplot() +
+      theme_minimal() +
+      scale_fill_manual(values = c("0" = "dodgerblue", "1" = "tomato")) +
+      labs(title = paste("Box Plot of", attribute, "by Infection Status"),
+           x = "infected",
+           y = attribute)
+  })
 }
 
--- ui.R
+++ ui.R
@@ -10,23 +10,44 @@
 library(shiny)
 library(leaflet)
 library(shinydashboard)
+library(ggplot2)
+library(dplyr)
+library(randomForest)
+library(rpart)
+library(nnet)
+library(caret)
+library(mlr)
+library(DT)
+library(DALEX)
+library(shinythemes)
+library(pROC)
+library(plotly)
 
 
 dashboardPage(
-  dashboardHeader(title = "Poor WEKA"),
+  dashboardHeader(title = "PWEKA (Poor WEKA)"),
   dashboardSidebar(
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+    ),
     sidebarMenu(
-      menuItem("Confusion matrix", tabName = "confusionMatrix", icon = icon("table")),
-      menuItem("Learning curves plot", tabName = "learningCurves", icon = icon("table")),
-      menuItem("Datatable", tabName = "dataTable", icon = icon("table"))
+      menuItem("Algorithms analizer", tabName = "algorithmsAnalizer", icon = icon("table")),
+      menuItem("Dataset analizer", tabName = "dataTable", icon = icon("th")),
+      menuItem("Fancy plot vizualizer", tabName = "fancyPlot", icon = icon("chart-line"))
     )
   ),
   dashboardBody(
+    theme = shinythemes::shinytheme("cerulean"),
     tabItems(
-      tabItem(tabName = "confusionMatrix",
+      tabItem(tabName = "algorithmsAnalizer",
               fluidRow(
-                column(3,
-                       selectInput("selectAlgorithm", label = h3("Select algorithm"),
+                column(4),
+                column(3, h3("Select algorithm to analyze")),
+              ),
+              fluidRow(
+                column(4),
+                column(6,
+                       selectInput("selectAlgorithm", label = NULL,
                                    choices = list("Random Forest" = "rf", 
                                                   "Decision Tree" = "dt",
                                                   "Logistic Regression" = "lr", 
@@ -34,34 +55,99 @@ dashboardPage(
                                    selected = "rf"))
               ),
               fluidRow(
-                column(5,
-                       plotOutput("confusionMatrix"))
-              )),
-      tabItem(tabName = "learningCurves",
-              fluidRow(
-                column(3,
-                       checkboxGroupInput("chooseAlgorithms", label = h3("Choose algorithm(s)"),
-                                   choices = list("Random Forest" = "rf", 
-                                                  "Decision Tree" = "dt",
-                                                  "Logistic Regression" = "lr", 
-                                                  "Neural Network" = "nn",
-                                                  "Support Vector Machine" = "svm"), 
-                                   selected = "rf"))
+                column(7, h3("Confusion matrix")),
+                column(5, h3("ROC curve")),
               ),
               fluidRow(
                 column(5,
-                       plotOutput("learning_curves"))
+                       plotOutput("confusionMatrix")),
+                column(2),
+                column(5,
+                       plotOutput("ROCcurve"))
               )),
       tabItem(tabName = "dataTable",
               fluidRow(
-                column(10,
-                       DTOutput("dataTableOutput"))
+                column(4),
+                column(5, h3("Datatable consisting all patients health data"))
+              ),
+              fluidRow(
+                column(12,
+                       DTOutput("dataTableOutput"))),
+              fluidRow(
+                column(6, 
+                       h3("Select attribute to see its distribution")),
+                column(6, 
+                       h3("Select a record from a table to see the features importance analysis"))
+              ),
+              fluidRow(
+                column(6,
+                       selectInput("selectColumn", label = NULL, choices = list(
+                         "time" = "time",
+                         "treatment" = "treatment",
+                         "age" = "age",
+                         "weight" = "weight",
+                         "hemophilia" = "hemophilia",
+                         "homosexual_activity" = "homosexual_activity",
+                         "drugs" = "drugs",
+                         "karnofsky_score" = "karnofsky_score",
+                         "oprior" = "oprior",
+                         "z30" = "z30",
+                         "days_before_anti_retrovial" = "days_before_anti_retrovial",
+                         "non_white" = "non_white",
+                         "gender" = "gender",
+                         "antiretroviral_history" = "antiretroviral_history",
+                         "strat" = "strat",
+                         "symptom" = "symptom",
+                         "treatment_ZDV" = "treatment_ZDV",
+                         "offtrt" = "offtrt",
+                         "CD4_baseline" = "CD4_baseline",
+                         "CD4_after_20days" = "CD4_after_20days",
+                         "CD8_baseline" = "CD8_baseline",
+                         "CD8_after_20days" = "CD8_after_20days",
+                         "infected" = "infected"),
+                         selected = "time")),
+                column(6,
+                     selectInput("selectAlgorithm2", label = "Select algorithm",
+                                 choices = list("Random Forest" = "Random Forest", 
+                                                "Decision Tree" = "Decision Tree",
+                                                "Logistic Regression" = "Logistic Regression", 
+                                                "Neural Network" = "Neural Network"), 
+                                 selected = "rf"))
               ),
               fluidRow(
                 column(5,
+                       plotOutput("selectedColumnPlot")),
+                column(1),
+                column(5,
                        plotOutput("selectedRowPlot"))
-              ))
-    )
+              )),
+      tabItem(tabName = "fancyPlot",
+              fluidRow(
+                column(4),
+                column(3, h3("Select attribute to visualize"))
+              ),
+              fluidRow(
+                column(4),
+                column(6,
+                       selectInput("selectAttribute", label = NULL, choices = list(
+                         "time" = "time",
+                         "age" = "age",
+                         "weight" = "weight",
+                         "days_before_anti_retrovial" = "days_before_anti_retrovial",
+                         "CD4_baseline" = "CD4_baseline",
+                         "CD4_after_20days" = "CD4_after_20days",
+                         "CD8_baseline" = "CD8_baseline",
+                         "CD8_after_20days" = "CD8_after_20days"),
+                         selected = "time"))
+              ),
+              fluidRow(
+                column(5,
+                       plotOutput("violinPlot")),
+                column(1),
+                column(5,
+                       plotOutput("Boxplot"))
+              )
+    ))
   )
 )
 
